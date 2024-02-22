04fd3915eb81b100b826a0e487849a965b7332f7
oussama achemlal <oussama.achemlal777@gmail.com>
2024-02-22 15:28:06 +0100

    UserLocationDTO class implementation

--- springBoot-dto/src/main/java/com/example/springBootdto/dto/UserLocationDTO.java
+++ springBoot-dto/src/main/java/com/example/springBootdto/dto/UserLocationDTO.java
@@ -1,5 +1,14 @@
 package com.example.springBootdto.dto;
 
+import lombok.Data;
+
+@Data
 public class UserLocationDTO {
+    private String userId;
+    private String email;
+    private String palce;
+
+    private double longitude;
+    private double latitude;
 
 }
