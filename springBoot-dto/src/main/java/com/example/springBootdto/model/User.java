812c26f32f6c5f1d5713f65434ccdbbc9d71a4e2
oussama achemlal <oussama.achemlal777@gmail.com>
2024-02-22 15:01:06 +0100

    entity User and Location created , and begin creation of the UserRepository

---
+++ springBoot-dto/src/main/java/com/example/springBootdto/model/User.java
@@ -0,0 +1,27 @@
+package com.example.springBootdto.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    private String password;
+    @ManyToOne(fetch = FetchType.EAGER, optional=false)
+    @JoinColumn(name = "location_id")
+    private Location location;
+}
---
+++ springBoot-dto/src/main/java/com/example/springBootdto/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.springBootdto.repository;
+
+public interface UserRepository {
+
+}
